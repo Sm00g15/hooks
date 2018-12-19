56283e88fbc3d8c247c520ac39298db637208ed5
Sean M <sean@arrowburn.co.nz>
2018-12-20 09:04:13 +1300

    added refs

--- react-news/src/App.js
+++ react-news/src/App.js
@@ -1,17 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 export default function App() {
   const [results, setResults] = useState([]);
-  useEffect(() => {
-    axios
-      .get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-      .then(response => {
-        console.log(response.data);
-        setResults(response.data.hits);
-      });
-  }, []);
+  const [query, setQuery] = useState('react hooks');
+  const searchInputRef = useRef();
+  const getResults = async () => {
+    const response = await axios.get(
+      `http://hn.algolia.com/api/v1/search?query=${query}`
+    );
+    setResults(response.data.hits);
+  };
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  };
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus();
+  };
   return (
     <>
+      <form action=''>
+        <input
+          value={query}
+          type='text'
+          onChange={event => setQuery(event.target.value)}
+          ref={searchInputRef}
+        />
+        <button type='submit' onClick={handleSearch}>
+          Search
+        </button>
+        <button type='button' onClick={handleClearSearch}>
+          Clear
+        </button>
+      </form>
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
